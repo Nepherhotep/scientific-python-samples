9c4a77098cfd7bf49c32117b871e6f8221473a98
Alexey Zankevich <zancudero@gmail.com>
2016-04-14 10:30:33 +0300

    updated sample with readline

--- working_with_files/part1/working_with_files_readline.py
+++ working_with_files/part1/working_with_files_readline.py
@@ -1,8 +1,17 @@
+# coding: utf-8
+
 f = open("multiline_sample.txt")
+
+result = 0
 
 line = f.readline()
 while line:
-    print(line)
+    # конвертируем строку в число
+    value = int(line)
+
+    # добавляем число к сумме
+    result += value
+
     line = f.readline()
 
 f.close()
